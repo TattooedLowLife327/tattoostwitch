b09277e39ba24b3e626c3284d0d09ff0b49a8cbf
TattooedLowLife <your@email.com>
2025-11-04 06:30:13 -0500

    fix: convert ecosystem config to ESM syntax

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,9 @@
-module.exports = {
+export default {
   apps: [{
     name: 'twitch-bot',
     script: './bot.js',
-    watch: true,
-    ignore_watch: [
-      'node_modules',
-      'netlify',
-      '*.html',
-      '*.md',
-      '.git',
-      'nul'
-    ],
+    watch: false,
     max_memory_restart: '200M',
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    env: {
-      NODE_ENV: 'production'
-    },
     autorestart: true,
     max_restarts: 10,
     min_uptime: '10s'
