ea9d07f0a9e2b7667f663765cf7234058aa4a7da
TattooedLowLife <your@email.com>
2025-11-04 19:36:23 -0500

    fix: simplify !dcd message to just name and URL

--- bot.js
+++ bot.js
@@ -502,7 +502,7 @@ client.on('message', async (channel, tags, message, self) => {
 
   // !dcd (Dead Center Darts promo)
   if (text.toLowerCase() === '!dcd') {
-    say('Dead Center Darts - Check us out at www.deadcenterdarts.com');
+    say('Dead Center Darts - www.deadcenterdarts.com');
     triggerPromo(0);
     console.log(`[DCD] Promo triggered by ${uname}`);
     return;
